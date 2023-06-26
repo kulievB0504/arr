712531e73f9d8f7ad01f410827cc04294bacf441
kulievB0504 <kbekzod2005@gmail.com>
2023-06-26 18:40:31 +0300

    index_arr

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-// let names = ["ayub", 22, "zafar", ["hello", [[[12]]], 12], ["axror", 122, "10101011"], 0, 12, 12, 3123.213213, true, false, {}, [], undefined, null, 21, NaN, ""]
+let names = ["ayub", 22, "zafar", ["hello", [[[12]]], 12], ["axror", 122, "10101011"], 0, 12, 12, 3123.213213, true, false, {}, [], undefined, null, 21, NaN, ""]
 
-// let total_names = confirm(`${names[1] + names[3][1][0][0][0] + names[3][2] + names[4][1] + names[5] + names[6] + names[7] + names[8] + names[15]}`)
+let total_names = confirm(`${names[1] + names[3][1][0][0][0] + names[3][2] + names[4][1] + names[5] + names[6] + names[7] + names[8] + names[15]}`)
 
 
 let arr = [[{a: { price: 20}}],[{a: { price: 35}}],[{a: { price: 44}}]]
 
+let total = confirm(`${
+    
+    arr[0][0].a.price + arr[1][0].a.price + arr[2][0].a.price
 
+}`)
 
 
 
