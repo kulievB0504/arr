a1cee1b5d4c7cbac7dbb92ccde69b5f84e9c3eff
kulievB0504 <kbekzod2005@gmail.com>
2023-06-26 18:23:05 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// let names = ["ayub", 22, "zafar", ["hello", [[[12]]], 12], ["axror", 122, "10101011"], 0, 12, 12, 3123.213213, true, false, {}, [], undefined, null, 21, NaN, ""]
+
+// let total_names = confirm(`${names[1] + names[3][1][0][0][0] + names[3][2] + names[4][1] + names[5] + names[6] + names[7] + names[8] + names[15]}`)
+
+
+let arr = [[{a: { price: 20}}],[{a: { price: 35}}],[{a: { price: 44}}]]
+
+
+
+
+
+
